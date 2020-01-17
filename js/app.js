ac5fa77221f63cadd641dd25877fed3e054095c0
DoaaEssam52 <32943933+DoaaEssam52@users.noreply.github.com>
2020-01-17 15:32:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,16 +50,21 @@ var txt = document.getElementById("shareText").textContent;
   }
 }(document, 'script', 'twitter-wjs');
 
+// زر المشاركة عبر الواتساب
 
 function whatsAppShare(id) {
   var txt = document.getElementById(id);
   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  if (isMobile) {
+  if (isMobile) 
+  {
     window.location.href = "whatsapp://send?text=" + txt.textContent + "Link: " + document.URL,false;
-    //window.location.href = "whatsapp://send?text=" + txt.textContent,true;
-  } else {
+  } 
+  else {
     var whatsAppWindow = window.open('https://web.whatsapp.com://send?text=' + txt.textContent + 'Link: ' + document.URL ,"_blank");
-    if (whatsAppWindow.focus) { whatsAppWindow.focus(); }
+    if (whatsAppWindow.focus)
+    {
+      whatsAppWindow.focus();
+    }
     return false;
   }
 }
